740350c07e855f1da786e31210274457a41ebafb
qb1968 <qb1968@aol.com>
2020-09-29 19:09:35 -0400

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import React from 'react';
-
-
+import Header from './components/Header'
+import './default.scss'
+import Homepage from './pages/Homepage'
 function App() {
   return (
     <div className="App">
-      <h2>Hello World</h2>
+      <Header/>
+      <div className="main">
+      <Homepage/>
+      </div>
     </div>
   );
 }
